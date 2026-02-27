ed7c52729520957a8f494ef065d5ed57b26f4002
Rudraksh Kumar <rudrakshkumar2908@gmail.com>
2026-02-27 20:18:59 +0530

    feat: integrate Prisma session store
    - Create lib/prisma.js for client setup
    - Add Session model to schema.prisma and run initial migration
    - Configure express-session in app.js using @quixo3/prisma-session-store and PrismaPg adapter

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require("dotenv").config({path: ".env"});
 const express = require("express");
+const expressSession = require("express-session");
 const passport = require("passport");
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+
+const {PrismaPg} = require('@prisma/adapter-pg');
+const {PrismaClient} = require("@prisma/client");
+const {PrismaSessionStore} = require("@quixo3/prisma-session-store")
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({connectionString});
+const prisma = new PrismaClient({adapter});
 
 const app = express();
 
@@ -12,6 +21,25 @@ app.use(express.urlencoded({extended: true}));
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+// Make session store
+
+app.use(expressSession({
+    cookie: {
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+        prisma,
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+}));
 
 app.use(indexRouter);
 
